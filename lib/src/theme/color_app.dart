6587c55e8027ca47d8504c5650211735d627e040
Mohamed Qayed <145319237+Mo7amedQ2d@users.noreply.github.com>
2024-11-25 16:25:39 +0300

    create theme app , and color app

---
+++ lib/src/theme/color_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  static const Color colorBackgroundLight = Color(0xffF2F2F2);
+  static const Color colorLight = Color(0xffFFFFFF);
+  static const Color colorPrimaryLight = Color(0xff008DC7);
+  static const Color colorSecondLight = Color(0xffD5D4D7);
+  static const Color colorTextPrLight = Color(0xff000000);
+  static const Color colorTextScLight = Color(0xffA5A29D);
+
+
+  static const Color colorBackgroundDark = Color(0xff202023);
+  static const Color colorDark = Color(0xff2D2F38);
+  static const Color colorPrimaryDark = Color(0xff3D5EFA);
+  static const Color colorSecondDark = Color(0xff4A4C56);
+  static const Color colorTextPrDark = Color(0xffFFFFFF);
+  static const Color colorTextScDark = Color(0xffA5A29D);
+
+}
---
+++ lib/src/theme/theme_app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ThemeApp {
+  final lightTheme =ThemeData.light().copyWith();
+  final darkTheme =ThemeData.dark().copyWith();
+}
